3ef6db791f937f7741ba6bb72e6c557c1716b547
Majed Alateeq <87226233+mjdateeq@users.noreply.github.com>
2022-10-10 03:57:56 -0600

    Update cfcm.py

--- cfcm.py
+++ cfcm.py
@@ -1,3 +1,5 @@
+# Not completed
+
 from sklearn.metrics import pairwise_distances_argmin
 from sklearn.datasets import make_blobs
 import matplotlib.pyplot as plt
@@ -10,24 +12,27 @@ y = [0.2, 0.67, 0.7, 0.45, 0.95, 1.05, 1.1, 2.5, 2.67, 2.7, 2.45, 2.95, 3.05, 3.
 data =  list(zip(x,y))
 num_data=len(data)
 num_feature=np.ndim((np.array(data)))
-
 # generate inital weights
 init_weight = np.random.rand(num_data, num_feature)
+k=2
+centroid = []
 
-
-def Def_Centroid (data, weight, num_cen, num_feature, p):
-    sumweight = np.sum(np.square(init_weight), axis=0)
+def Def_Centroid (data, weight, num_cen, num_feature):
     centroid=np.zeros((num_cen,num_feature))
+
     for i in range (num_cen):
         for j in range (num_feature):
-            
-            sumMM=sum((weight(:,i)**p)* data(:,j));
-            centroid(i,j)=sumMM/SS; 
+            SS=np.sum(np.square(weight[:,i]))
+            sumMM=np.sum(np.square(init_weight[:,i])*data[:,j])
+            centroid[i,j]=sumMM/SS
 
+    return centroid
 
+arr = np.array (data)
+centroid = Def_Centroid (arr, init_weight, k, num_feature)
 
 plt.scatter(x, y, c=labels, s=50, cmap='viridis')
-plt.scatter(centers[0][0],centers[0][1],s=50, marker='x')
-plt.scatter(centers[1][0],centers[1][0],s=50, marker='x')
+plt.scatter(centroid[0][0],centroid[0][1],s=50, marker='x')
+plt.scatter(centroid[1][0],centroid[1][0],s=50, marker='x')
 
 plt.show()
